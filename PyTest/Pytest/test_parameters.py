4cd0e5e52a9673d262068a9fa1eab569a8c21a96
joris.verhulst <joris.verhulst@medicim323.medicim.local>
2022-08-26 15:37:02 +0200

    test

--- PyTest/Pytest/test_parameters.py
+++ PyTest/Pytest/test_parameters.py
@@ -3,7 +3,7 @@ import pytest
 def sum(a,b):
     return a +b
 
-@pytest.mark.parametrize("input1,input2,output", [(3,5,8),(6,9,15)])
+@pytest.mark.parametrize("input1,input2,output", [(3,6,8),(6,9,15)])
                       
 def test_calc_sum_1(input1,input2,output):
     result = sum(input1,input2)
